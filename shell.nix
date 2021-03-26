aef027b498c9317b0a3d3acd2b30e7fb71082c55
johnpyp <johnpyp.dev@gmail.com>
2021-03-25 21:01:41 -0400

    Remove commented shell.nix stuff

--- shell.nix
+++ shell.nix
@@ -7,21 +7,7 @@ stdenv.mkDerivation {
   name = "rust-env";
   buildInputs = [
     rustup
-    # # Note: to use use stable, just replace `nightly` with `stable`
-    # nixpkgs.latest.rustChannels.nightly.rust
-    # nixpkgs.latest.rustChannels.nightly.cargo
-    # nixpkgs.latest.rustChannels.nightly.rust-src
-
-    # Add some extra dependencies from `pkgs`
     nixpkgs.pkgconfig
     nixpkgs.openssl
   ];
-
-
-  # shellHook = ''
-  #   export RUST_SRC_PATH="${rustChannel.rust-src}/lib/rustlib/src/rust/library"
-  # '';
-
-  # Set Environment Variables
-  # RUST_BACKTRACE = 1;
 }
